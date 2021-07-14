28d3dfb30e34445ec6bf1fe111314f4df6ee9314
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-07-14 17:08:15 -0600

    various updates

--- dhrystone/src/sl_config.h
+++ dhrystone/src/sl_config.h
@@ -6,15 +6,16 @@
 #ifndef SL_CONFIG_H_
 #define SL_CONFIG_H_
 
-#define SL_CONFIG_VERSION_STRING "0.12"
-#define SL_CONFIG_VERSION_BCD 0x0C
+#define SL_CONFIG_VERSION_STRING "1.0"
+#define SL_CONFIG_VERSION_BCD 0x100
 #define SL_CONFIG_DOCUMENT_ID "QpXcn3w2P1YUcatvAZZd"
 #define SL_CONFIG_TEAM_ID ""
 #define SL_CONFIG_NAME "dhrystone"
 #define SL_CONFIG_TYPE "app"
-#define SL_CONFIG_PUBLISHER ""
 #define SL_CONFIG_PERMISSIONS "public"
 #define SL_CONFIG_HARDWARE_ID_STRING ""
+
+//Thread stack sizes
 
 #endif
 
--- fstest/src/sl_config.h
+++ fstest/src/sl_config.h
@@ -6,14 +6,16 @@
 #ifndef SL_CONFIG_H_
 #define SL_CONFIG_H_
 
-#define SL_CONFIG_VERSION_STRING "0.1"
-#define SL_CONFIG_VERSION_BCD 0x01
-#define SL_CONFIG_DOCUMENT_ID ""
-#define SL_CONFIG_NAME "drivetest"
+#define SL_CONFIG_VERSION_STRING "1.0"
+#define SL_CONFIG_VERSION_BCD 0x100
+#define SL_CONFIG_DOCUMENT_ID "yWqzp51297PMiDDOJDKt"
+#define SL_CONFIG_TEAM_ID ""
+#define SL_CONFIG_NAME "fstest"
 #define SL_CONFIG_TYPE "app"
-#define SL_CONFIG_PUBLISHER ""
 #define SL_CONFIG_PERMISSIONS "public"
 #define SL_CONFIG_HARDWARE_ID_STRING ""
+
+//Thread stack sizes
 
 #endif
 
